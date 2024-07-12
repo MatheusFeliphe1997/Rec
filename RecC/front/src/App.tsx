a4278e9dedb2283420a59d6f6b2d5c428431367b
MATHEUS <matheus.feliphe@hotmail.com.br>
2024-07-12 11:55:10 -0300

    inicio do. front
    inicio do front

---
+++ RecC/front/src/App.tsx
@@ -0,0 +1,57 @@
+
+import "./App.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <div>
+//         <BrowserRouter>
+//           <nav>
+//             <ul>
+//               <li>
+//                 <Link to={"/"}>Home</Link>
+//               </li>
+//               <li>
+//                 <Link to={"/pages/funcionario/listar"}>
+//                   Listar Funcionarios{" "}
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link to={"/pages/tarefa/listarconcluidas"}>
+//                   Listar Tarefas Concluídas{" "}
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link to={"/pages/tarefa/listarnaoconcluidas"}>
+//                   Listar Tarefas Não Concluídas{" "}
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link to={"/pages/tarefa/cadastrar"}>
+//                   Cadastrar Tarefa{" "}
+//                 </Link>
+//               </li>
+//             </ul>
+//           </nav>
+//           <Routes>
+//             <Route path="/" element={<ListarTarefas />} />
+//             <Route
+//               path="/pages/tarefa/listar"
+//               element={<ListarTarefas />}
+//             />
+//             <Route
+//               path="/pages/tarefa/listarconcluidas"
+//               element={<ListarTarefasConcluidas />}
+         
+           
+//           </Routes>
+//           <footer>
+//             <p>Desenvolvido por Matheus Feliphe</p>
+//           </footer>
+//         </BrowserRouter>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
