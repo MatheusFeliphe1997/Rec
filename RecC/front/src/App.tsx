907d98a9d7af694655deab86b5bdb182b6b13c88
MATHEUS <matheus.feliphe@hotmail.com.br>
2024-07-12 12:00:03 -0300

    algo
    algo

--- RecC/front/src/App.tsx
+++ RecC/front/src/App.tsx
@@ -1,57 +1,56 @@
 
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-// function App() {
-//   return (
-//     <div>
-//       <div>
-//         <BrowserRouter>
-//           <nav>
-//             <ul>
-//               <li>
-//                 <Link to={"/"}>Home</Link>
-//               </li>
-//               <li>
-//                 <Link to={"/pages/funcionario/listar"}>
-//                   Listar Funcionarios{" "}
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link to={"/pages/tarefa/listarconcluidas"}>
-//                   Listar Tarefas Concluídas{" "}
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link to={"/pages/tarefa/listarnaoconcluidas"}>
-//                   Listar Tarefas Não Concluídas{" "}
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link to={"/pages/tarefa/cadastrar"}>
-//                   Cadastrar Tarefa{" "}
-//                 </Link>
-//               </li>
-//             </ul>
-//           </nav>
-//           <Routes>
-//             <Route path="/" element={<ListarTarefas />} />
-//             <Route
-//               path="/pages/tarefa/listar"
-//               element={<ListarTarefas />}
-//             />
-//             <Route
-//               path="/pages/tarefa/listarconcluidas"
-//               element={<ListarTarefasConcluidas />}
-         
-           
-//           </Routes>
-//           <footer>
-//             <p>Desenvolvido por Matheus Feliphe</p>
-//           </footer>
-//         </BrowserRouter>
-//       </div>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <div>
+        <BrowserRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to={"/"}>Home</Link>
+              </li>
+              <li>
+                <Link to={"/pages/aluno/listar"}>
+                  Listar Alunos{" "}
+                </Link>
+              </li>
+              <li>
+                <Link to={"/pages/imc/listar"}>
+                  Listar Imc{" "}
+                </Link>
+              </li>
+                <li>
+                    <Link to={"/pages/imc/cadastrar"}>
+                    Cadastrar Imc{" "}
+                    </Link>
+              <li>
+                <Link to={"/pages/aluno/cadastrar"}>
+                  Cadastrar Tarefa{" "}
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<ListarImc />} />
+            <Route
+              path="/pages/tarefa/listar"
+              element={<ListarTarefas />}
+            />
+            <Route
+              path="/pages/tarefa/listarconcluidas"
+              element={<ListarTarefasConcluidas />}
+            />
+          </Routes>
+          <footer>
+            <p>Desenvolvido por Matheus Feliphe</p>
+          </footer>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
